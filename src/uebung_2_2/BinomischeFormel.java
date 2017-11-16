42c98c124829bb307f75c8303fe8c7993b579493
15128 <15128@A001-10.nordakademie.intern>
2017-11-16 11:34:24 +0100

    Binomische Formel berechnen

---
+++ src/uebung_2_2/BinomischeFormel.java
@@ -0,0 +1,23 @@
+package uebung_2_2;
+
+public class BinomischeFormel { 
+ 
+ 
+ 	public static void main(String[] args) { 
+ 		double a = 8; 
+ 		double b = 3; 
+ 
+
+ 		// (a+b)^2 = a^2 + 2ab + b^2 
+ 		double binomLinks = Math.pow((a + b), 2); 
+ 		double binomRechts = Math.pow(a, 2) + 2 * a * b + Math.pow(b, 2); 
+ 
+
+ 		System.out.println("(a+b)^2 = " + binomLinks); 
+ 		System.out.println("a^2 + 2ab + b^2 = " + binomRechts); 
+ 	} 
+ 
+ 
+ } 
+
+
